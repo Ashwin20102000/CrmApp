316fbfd9f9a09e8105a55ca90cd4775f0bcd6aa7
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2021-08-24 20:57:01 +0530

    Add files via upload

---
+++ crm-app/src/manager-message/manager-message.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManagerMessageComponent } from './manager-message.component';
+
+describe('ManagerMessageComponent', () => {
+  let component: ManagerMessageComponent;
+  let fixture: ComponentFixture<ManagerMessageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ManagerMessageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ManagerMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
